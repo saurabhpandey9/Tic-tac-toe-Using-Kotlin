45c3f13f3139e120ace92e54f5590448e80ca57b
Saurabh Pandey <saurabh17100@gmail.com>
2021-01-11 00:09:39 +0530

    UI enhanced and Multilevel game added

--- app/src/main/java/com/developerdesk9/tictactoe/MainActivity.kt
+++ app/src/main/java/com/developerdesk9/tictactoe/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.developerdesk9.tictactoe
 
 import android.graphics.Color
+import android.media.Image
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    var player1Score = 0
+    var player2Score= 0
+
+    var currentPlayer =1
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
 
     }
-
 
 
     fun onBtnClick(view: View){
@@ -39,16 +51,10 @@ class MainActivity : AppCompatActivity() {
             R.id.button9 -> 9
             else -> -1
         }
-
         positionUpdate(curBtnId,view)
-
-
     }
 
-    var currentPlayer =1
 
-    var player1 = ArrayList<Int>()
-    var player2 = ArrayList<Int>()
 
     fun positionUpdate(curBtnId:Int,view: View){
         var BTN = view as Button
@@ -75,7 +81,7 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        view.setOnClickListener(null)
+        view.isEnabled = false
         winner()
 
     }
@@ -84,74 +90,242 @@ class MainActivity : AppCompatActivity() {
 
         // row 1
         if (player1.contains(1) && player1.contains(2) && player1.contains(3)){
-            Toast.makeText(applicationContext,"PLayer 1 Won",Toast.LENGTH_SHORT).show()
+            alertDialog(1)
         }
-        if (player2.contains(1) && player2.contains(2) && player2.contains(3)){
-            Toast.makeText(applicationContext,"PLayer 2 Won",Toast.LENGTH_SHORT).show()
+        else if (player2.contains(1) && player2.contains(2) && player2.contains(3)){
+            alertDialog(2)
         }
 
         // row 2
-        if (player1.contains(4) && player1.contains(5) && player1.contains(6)){
-            Toast.makeText(applicationContext,"PLayer 1 Won",Toast.LENGTH_SHORT).show()
+        else if (player1.contains(4) && player1.contains(5) && player1.contains(6)){
+            alertDialog(1)
         }
-        if (player2.contains(4) && player2.contains(5) && player2.contains(6)){
-            Toast.makeText(applicationContext,"PLayer 2 Won",Toast.LENGTH_SHORT).show()
+        else if (player2.contains(4) && player2.contains(5) && player2.contains(6)){
+            alertDialog(2)
         }
 
         // row 3
-        if (player1.contains(7) && player1.contains(8) && player1.contains(9)){
-            Toast.makeText(applicationContext,"PLayer 1 Won",Toast.LENGTH_SHORT).show()
+        else if (player1.contains(7) && player1.contains(8) && player1.contains(9)){
+            alertDialog(1)
         }
-        if (player2.contains(7) && player2.contains(8) && player2.contains(9)){
-            Toast.makeText(applicationContext,"PLayer 2 Won",Toast.LENGTH_SHORT).show()
+        else if (player2.contains(7) && player2.contains(8) && player2.contains(9)){
+            alertDialog(2)
         }
 
 
 
 
         // column 1
-        if (player1.contains(1) && player1.contains(4) && player1.contains(7)){
-            Toast.makeText(applicationContext,"PLayer 1 Won",Toast.LENGTH_SHORT).show()
+        else if (player1.contains(1) && player1.contains(4) && player1.contains(7)){
+            alertDialog(1)
         }
-        if (player2.contains(1) && player2.contains(4) && player2.contains(7)){
-            Toast.makeText(applicationContext,"PLayer 2 Won",Toast.LENGTH_SHORT).show()
+        else if (player2.contains(1) && player2.contains(4) && player2.contains(7)){
+            alertDialog(2)
         }
 
         // column 2
-        if (player1.contains(2) && player1.contains(5) && player1.contains(8)){
-            Toast.makeText(applicationContext,"PLayer 1 Won",Toast.LENGTH_SHORT).show()
+        else if (player1.contains(2) && player1.contains(5) && player1.contains(8)){
+            alertDialog(1)
         }
-        if (player2.contains(2) && player2.contains(5) && player2.contains(8)){
-            Toast.makeText(applicationContext,"PLayer 2 Won",Toast.LENGTH_SHORT).show()
+        else if (player2.contains(2) && player2.contains(5) && player2.contains(8)){
+            alertDialog(2)
         }
 
         // column 3
-        if (player1.contains(3) && player1.contains(6) && player1.contains(9)){
-            Toast.makeText(applicationContext,"PLayer 1 Won",Toast.LENGTH_SHORT).show()
+        else if (player1.contains(3) && player1.contains(6) && player1.contains(9)){
+            alertDialog(1)
         }
-        if (player2.contains(3) && player2.contains(6) && player2.contains(9)){
-            Toast.makeText(applicationContext,"PLayer 2 Won",Toast.LENGTH_SHORT).show()
+        else if (player2.contains(3) && player2.contains(6) && player2.contains(9)){
+            alertDialog(2)
         }
 
 
-        // diagonal 3
-        if (player1.contains(1) && player1.contains(5) && player1.contains(9)){
-            Toast.makeText(applicationContext,"PLayer 1 Won",Toast.LENGTH_SHORT).show()
+        // diagonal 1
+        else if (player1.contains(1) && player1.contains(5) && player1.contains(9)){
+            alertDialog(1)
         }
-        if (player2.contains(1) && player2.contains(5) && player2.contains(9)){
-            Toast.makeText(applicationContext,"PLayer 2 Won",Toast.LENGTH_SHORT).show()
+        else if (player2.contains(1) && player2.contains(5) && player2.contains(9)){
+            alertDialog(2)
         }
-        // diagonal 3
-        if (player1.contains(3) && player1.contains(5) && player1.contains(7)){
-            Toast.makeText(applicationContext,"PLayer 1 Won",Toast.LENGTH_SHORT).show()
+
+        // diagonal 2
+        else if (player1.contains(3) && player1.contains(5) && player1.contains(7)){
+            alertDialog(1)
         }
-        if (player2.contains(3) && player2.contains(5) && player2.contains(7)){
-            Toast.makeText(applicationContext,"PLayer 2 Won",Toast.LENGTH_SHORT).show()
+        else if (player2.contains(3) && player2.contains(5) && player2.contains(7)){
+            alertDialog(2)
+        }
+
+        else if ( ((player1.size)+(player2.size))==9){
+            noOnewon()
         }
 
     }
 
 
+    fun noOnewon(){
 
+        var mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_alert_layout,null)
+
+
+        var won_txt = mDialogView.findViewById<TextView>(R.id.tv_alert_dialog_won_show)
+
+
+        val mBuilder = AlertDialog.Builder(this).setView(mDialogView)
+
+        var btn_reply = mDialogView.findViewById<Button>(R.id.btn_replay)
+        var btn_exit = mDialogView.findViewById<Button>(R.id.btn_exit_app)
+
+
+        var alert_d_imag = mDialogView.findViewById<ImageView>(R.id.alert_d_imag)
+        var alert_dialg_congo = mDialogView.findViewById<TextView>(R.id.alert_dialg_congo)
+
+        alert_d_imag.setImageResource(R.drawable.oops)
+        alert_dialg_congo.visibility = View.INVISIBLE
+
+        var tv_alert_dialog_won_show = mDialogView.findViewById<TextView>(R.id.tv_alert_dialog_won_show)
+        tv_alert_dialog_won_show.text = "Oops!! No one won"
+
+        var mAlertDialog = mBuilder.show()
+        mAlertDialog.setCanceledOnTouchOutside(false)
+
+
+
+        btn_exit.setOnClickListener {
+            mAlertDialog.dismiss()
+
+            finish()
+        }
+
+
+        btn_reply.setOnClickListener {
+            mAlertDialog.dismiss()
+            reset()
+
+        }
+
+
+
+
+    }
+
+    fun alertDialog(wonPlayer:Int){
+
+        if (wonPlayer==1) player1Score+=1 else player2Score+=1
+
+        var mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_alert_layout,null)
+
+
+        var won_txt = mDialogView.findViewById<TextView>(R.id.tv_alert_dialog_won_show)
+
+        won_txt.text = "Player : $wonPlayer Won"
+        tv_chance.text = "Chance of Player: $wonPlayer"
+
+
+        val mBuilder = AlertDialog.Builder(this).setView(mDialogView)
+
+        var mAlertDialog = mBuilder.show()
+
+        mAlertDialog.setCanceledOnTouchOutside(false)
+
+
+        var btn_exit = mDialogView.findViewById<Button>(R.id.btn_exit_app)
+        btn_exit.setOnClickListener {
+            mAlertDialog.dismiss()
+
+            finish()
+        }
+
+        var btn_reply = mDialogView.findViewById<Button>(R.id.btn_replay)
+        btn_reply.setOnClickListener {
+            mAlertDialog.dismiss()
+            reset()
+
+
+        }
+
+        tv_score_player1.text = "Player 1: $player1Score"
+        tv_score_player2.text = "Player 2: $player2Score"
+
+        currentPlayer = wonPlayer
+    }
+
+    fun reset(){
+        player1.clear()
+        player2.clear()
+
+        button1.text =""
+        button2.text =""
+        button3.text =""
+        button4.text =""
+        button5.text =""
+        button6.text =""
+        button7.text =""
+        button8.text =""
+        button9.text =""
+
+        button1.setBackgroundColor(getColor(R.color.btn_default_color))
+        button2.setBackgroundColor(getColor(R.color.btn_default_color))
+        button3.setBackgroundColor(getColor(R.color.btn_default_color))
+        button4.setBackgroundColor(getColor(R.color.btn_default_color))
+        button5.setBackgroundColor(getColor(R.color.btn_default_color))
+        button6.setBackgroundColor(getColor(R.color.btn_default_color))
+        button7.setBackgroundColor(getColor(R.color.btn_default_color))
+        button8.setBackgroundColor(getColor(R.color.btn_default_color))
+        button9.setBackgroundColor(getColor(R.color.btn_default_color))
+
+        button1.isEnabled = true
+        button2.isEnabled = true
+        button3.isEnabled = true
+        button4.isEnabled = true
+        button5.isEnabled = true
+        button6.isEnabled = true
+        button7.isEnabled = true
+        button8.isEnabled = true
+        button9.isEnabled = true
+
+
+
+    }
+
+
+    override fun onBackPressed() {
+        var mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_alert_layout,null)
+
+
+        var won_txt = mDialogView.findViewById<TextView>(R.id.tv_alert_dialog_won_show)
+
+
+        val mBuilder = AlertDialog.Builder(this).setView(mDialogView)
+
+        var btn_reply = mDialogView.findViewById<Button>(R.id.btn_replay)
+        var btn_exit = mDialogView.findViewById<Button>(R.id.btn_exit_app)
+
+        btn_reply.text ="NO"
+        btn_exit.text = "YES"
+
+        var alert_d_imag = mDialogView.findViewById<ImageView>(R.id.alert_d_imag)
+        var alert_dialg_congo = mDialogView.findViewById<TextView>(R.id.alert_dialg_congo)
+
+        alert_d_imag.setImageResource(R.drawable.exit)
+        alert_dialg_congo.visibility = View.INVISIBLE
+
+        var mAlertDialog = mBuilder.show()
+        mAlertDialog.setCanceledOnTouchOutside(false)
+
+
+
+        btn_exit.setOnClickListener {
+            mAlertDialog.dismiss()
+
+            finish()
+        }
+
+
+        btn_reply.setOnClickListener {
+            mAlertDialog.dismiss()
+
+        }
+    }
 
 }
